8ea9ec6aadb49f96a34c4b311955b3f4de42d9a7
dimmerz92 <andrew.weymes@outlook.com>
2022-09-03 08:20:32 +0800

    add 3 buttons with eventhandling to print result to screen

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     //plays a single round of rock, paper, scissors against the computer by taking in a player and computer argument
-    let winString = `You win! ${playerChoice} beats ${computerChoice}`;
-    let loseString = `You lose! ${computerChoice} beats ${playerChoice}`;
+    const winString = `You win! ${playerChoice} beats ${computerChoice}`;
+    const loseString = `You lose! ${computerChoice} beats ${playerChoice}`;
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == "rock" && computerChoice == "scissors") {
         return winString;
@@ -32,23 +32,32 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function game() {
+const selections = document.querySelectorAll(".selection");
+selections.forEach(selection => {
+    selection.addEventListener("click", () => {
+        const elmnt = document.getElementsByClassName("result");
+        const winner = playRound(selection.id, getComputerChoice());
+        document.getElementById("result").textContent = winner;
+    });
+});
+
+//function game() {
     //plays a 5 round game of rock, paper, scissors and keeps and reports the score at the end
-    let playerWinCount = 0;
-    let computerWinCount = 0;
-    for (let i = 0; i < 5; i++) {
-        let round = playRound(prompt("Enter a choice of rock, paper, or scissors"), getComputerChoice());
-        if (round.includes("You win!")) {
-            playerWinCount++;
-        } else if (round.includes("You lose!")) {
-            computerWinCount++;
-        }
-    }
-    if (playerWinCount > computerWinCount) {
-        return `You won! You got ${playerWinCount} and the computer got ${computerWinCount}`;
-    } else if (playerWinCount < computerWinCount) {
-        return `You lost! You got ${playerWinCount} and the computer got ${computerWinCount}`;
-    } else {
-        return "It appears to have been a draw!"
-    }
-}
+    //let playerWinCount = 0;
+    //let computerWinCount = 0;
+    //for (let i = 0; i < 5; i++) {
+    //    let round = playRound(prompt("Enter a choice of rock, paper, or scissors"), getComputerChoice());
+    //    if (round.includes("You win!")) {
+    //        playerWinCount++;
+    //    } else if (round.includes("You lose!")) {
+    //        computerWinCount++;
+    //    }
+    //}
+    //if (playerWinCount > computerWinCount) {
+    //    return `You won! You got ${playerWinCount} and the computer got ${computerWinCount}`;
+    //} else if (playerWinCount < computerWinCount) {
+    //    return `You lost! You got ${playerWinCount} and the computer got ${computerWinCount}`;
+    //} else {
+    //    return "It appears to have been a draw!"
+    //}
+//}
